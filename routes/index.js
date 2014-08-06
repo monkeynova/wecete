8c72312ddc646a3abd439816ef20f46bd1818cc1
Keith Peters <keith@monkeynova.com>
2014-08-06 16:01:33 -0700

    basic achievement layout

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WeCete' });
+};
+
+exports.achievement = function(req,res){
+  res.render('achievement', { title : 'You\'ve got Mail!', description : 'Receive an email asking if the user\'s email is working' } );
 };
