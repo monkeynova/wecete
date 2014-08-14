3b3622a2dcf43b068e3baf61f0abc22540fed785
Keith Peters <keith@monkeynova.com>
2014-08-14 13:47:47 -0700

    have counts for collections on user page

--- routes/index.js
+++ routes/index.js
@@ -44,15 +44,42 @@ exports.user = function(req,res)
             user = users[0] || {};
 	    user.owner = user.id;
 	    user.editable = site.canWrite( req, user );
-	    return db.denodeAll( "SELECT * FROM collections WHERE owner=$id;", { $id : user.id } );
+	    return Promise.all
+	    ([
+		db.denodeAll( "SELECT * FROM collections WHERE owner=$user;", { $user : user.id } ),
+		db.denodeAll
+		(
+		    "SELECT collection,count(id) as count FROM achievements" +
+		    "  WHERE collection IN ( SELECT id FROM collections WHERE owner=$user) GROUP BY collection;",
+		    { $user : user.id }
+		),
+		db.denodeAll
+		(
+		    "SELECT collection,count(achievement) as haves FROM haves" +
+		    "  LEFT OUTER JOIN achievements on achievements.id = haves.achievement" +
+		    "  WHERE achievements.collection IN ( SELECT id FROM collections WHERE owner=$user) GROUP BY collection;",
+		    { $user : user.id }
+		)
+	    ]);
 	},
 	site.htmlErrorSender( res )
     )
     .then
     (
-	function (collections)
+	function (results)
         {
+	    var collections = results[0] || [];
+	    var counts = results[1] || [];
+	    var haves = results[2] || [];
+
+	    var idToCounts = {};
+	    collections.forEach( function(c) { idToCounts[c.id] = {} } );
+	    counts.forEach( function(c) { if ( idToCounts[c.collection] ) { idToCounts[c.collection].count = c.count; } } );
+	    haves.forEach( function(h) { if ( idToCounts[h.collection] ) { idToCounts[h.collection].haves = h.haves; } } );
+
 	    user.collections = collections.filter( function(c) { return site.canRead( req, c ); } );
+	    user.collections.forEach( function(c) { counts = idToCounts[c.id] || {}; c.haves = counts.haves; c.count = counts.count; } );
+
 	    res.render('user', { site : siteData, user : user } );
 	},
 	site.htmlErrorSender( res )
